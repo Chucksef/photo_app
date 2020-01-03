79bfcdcb9d7b267fa42ffbb7e193d81981a3278a
chucksef <chucksef@gmail.com>
2020-01-03 14:55:24 -0700

    Added dynamic page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def get_page_title(page_title = '')
+        base_title = "Photo DB"
+        if page_title.empty?
+          base_title
+        else
+          page_title + " | " + base_title
+        end
+      end
 end
