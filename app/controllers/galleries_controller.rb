5b77653c71600372f187a9b6e93ad5931b2b411e
chucksef <chucksef@gmail.com>
2020-01-06 13:52:44 -0700

    Galleries#Edit Displays

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -11,6 +11,10 @@ class GalleriesController < ApplicationController
     def new
         @gallery = Gallery.new
     end
+        
+    def edit
+        @gallery = Gallery.find(params[:id])
+    end
 
     def create
         @gallery = Gallery.new(gallery_params)
