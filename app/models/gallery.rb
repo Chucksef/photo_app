dca05f203c47f5a08d116d284532485b5ce47ac7
chucksef <chucksef@gmail.com>
2020-01-07 10:34:06 -0700

    Invalid Galleries cannot be added to DB

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,2 +1,9 @@
 class Gallery < ApplicationRecord
+    #has_many :images
+
+    validates :name, presence: true
+    validates :name, length: { maximum: 20 }
+    validates :description, length: { maximum: 200 }
+    validates :order, numericality: true
+    validates :order, uniqueness: true
 end
