227ccba681783b3f89018b556bc4871d0a458ed8
chucksef <chucksef@gmail.com>
2020-01-07 10:53:52 -0700

    Website communicates form submission errors to user

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -3,7 +3,9 @@ class Gallery < ApplicationRecord
 
     validates :name, presence: true
     validates :name, length: { maximum: 20 }
+    validates :description, presence: true
     validates :description, length: { maximum: 200 }
     validates :order, numericality: true
     validates :order, uniqueness: true
+    
 end
