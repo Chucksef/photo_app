7b9107aa772d3b11df82e6fe489c3321ffae5718
chucksef <chucksef@gmail.com>
2020-01-22 13:13:18 -0700

    Basic user validation complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
 
+	validates                   :name, 
+									presence: true,
+									length: { maximum: 50 }
+	validates                   :email,
+									presence: true,
+									length: { maximum: 200 },
+									format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i },
+									uniqueness: { case_sensitive: false }
+	validates                   :password, presence: true, length: { minimum: 8 }
+	validates					:password_confirmation, presence: true
+	validates_confirmation_of	:password
+
 
 end
