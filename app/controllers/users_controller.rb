36f378e9ddb46087cd0190b24f9e353bd8d0e18e
chucksef <chucksef@gmail.com>
2020-01-22 12:38:47 -0700

    Added users#destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,10 @@ class UsersController < ApplicationController
   end
 
   def destroy
-      @user = User.find(params[:id])
+    @user = User.find(params[:id])
+
+    @user.destroy
+    redirect_to users_path
   end
 
   private def user_params
