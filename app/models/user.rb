6136e265fa839f348e4309bb5e383ce1c69255be
chucksef <chucksef@gmail.com>
2020-01-23 17:44:33 -0700

    added validation to password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,18 @@ class User < ApplicationRecord
 
 	before_save { self.email = email.downcase}
 
-	validates                   :name, 
-									presence: true,
-									length: { maximum: 50 }
-	validates                   :email,
-									presence: true,
-									length: { maximum: 200 },
-									format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i },
-									uniqueness: { case_sensitive: false }
+	validates   :name, 
+					presence: true,
+					length: { maximum: 50 }
+	validates	:email,
+					presence: true,
+					length: { maximum: 200 },
+					format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i },
+					uniqueness: { case_sensitive: false }
+	validates	:password,
+					presence: true,
+					length: { minimum: 6 }
+
 	has_secure_password
+	
 end
