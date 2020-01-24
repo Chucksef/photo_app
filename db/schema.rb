d715c1b1b1ca5a9fa883a7a1783b9596d75c8c40
chucksef <chucksef@gmail.com>
2020-01-23 17:08:51 -0700

    added index and uniqueness to users email in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_21_003640) do
+ActiveRecord::Schema.define(version: 2020_01_24_000715) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_01_21_003640) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_confirmation"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
