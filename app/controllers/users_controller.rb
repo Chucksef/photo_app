3b58de117102eab30caad76424bf69f4e1a5404b
chucksef <chucksef@gmail.com>
2020-01-25 10:43:17 -0700

    removed 'admin' from strong password parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
   end
 
   private def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
