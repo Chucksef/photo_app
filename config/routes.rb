903e0f95e9adfb0f5ea211056825c98915584e84
chucksef <chucksef@gmail.com>
2020-01-25 10:57:31 -0700

    remapped user#new to /signup, edited form for clarity, and streamlined page appearance

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/new'
-  get 'users/create'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root 'static_pages#home'
 
   get '/contact', to: "static_pages#contact"
@@ -22,5 +14,6 @@ Rails.application.routes.draw do
   end
 
   resources :users
+  get '/signup', to: "users#new"
 
 end
