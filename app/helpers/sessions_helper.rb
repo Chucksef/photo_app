938b1140b5d6860394de6eb6e9436b518b4d43cd
chucksef <chucksef@gmail.com>
2020-01-26 20:10:01 -0700

    logging out now works for permanent cookies

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,8 +11,15 @@ module SessionsHelper
     end
 
     def log_out
+        forget(current_user)
         session.delete(:user_id)
         @current_user = nil
+    end
+
+    def forget(user)
+        user.forget
+        cookies.delete(:user_id)
+        cookies.delete(:remember_token)
     end
 
     def current_user
