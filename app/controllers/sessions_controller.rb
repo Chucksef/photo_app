4491bf2b8520b69003b8cde39d7fc4ef75ce583c
chucksef <chucksef@gmail.com>
2020-01-26 19:55:52 -0700

    Updated #current_user in sessions_helper to check for permanent cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,21 @@
 class SessionsController < ApplicationController
-  def new
-  end
+	def new
+	end
 
-  def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-    log_in user
-    remember user
-		redirect_to user
-	else
-		flash.now[:danger] = "Invalid Email/Password combination"
-    	render :new
-    end
-  end
-  
-  def destroy
+	def create
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			log_in user
+			remember user
+			redirect_to user
+		else
+			flash.now[:danger] = "Invalid Email/Password combination"
+			render :new
+		end
+	end
+	
+	def destroy
 	log_out
 	redirect_to root_url
-  end
+	end
 end
