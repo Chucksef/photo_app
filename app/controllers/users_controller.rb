fdae42307444d5b23b25f0aed70ec2f5605cc4b5
chucksef <chucksef@gmail.com>
2020-01-28 11:37:45 -0700

    Users can no longer edit each other's profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 	before_action :logged_in_user, only: [:edit, :update]
+	before_action :correct_user, only: [:edit, :update]
   
 	def index
 		@users = User.all
@@ -60,6 +61,11 @@ class UsersController < ApplicationController
 				flash[:danger] = "Please Log In"
 				redirect_to login_url
 			end
+		end
+
+		def correct_user
+			@user = User.find(params[:id])
+			redirect_to(root_url) unless current_user?(@user)
 		end
 
 end
