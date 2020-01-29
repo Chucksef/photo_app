a501a55b59aa516e4885dd753bb0e2ef1d34d07b
chucksef <chucksef@gmail.com>
2020-01-29 10:11:41 -0700

    Admins can now edit other users' profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,7 +66,7 @@ class UsersController < ApplicationController
 
 		def correct_user
 			@user = User.find(params[:id])
-			redirect_to(root_url) unless current_user?(@user)
+			redirect_to(root_url) unless current_user?(@user) || user_admin?
 		end
 
 end
