0e044143fff74b4f9e7d62ea004711b9672205d7
chucksef <chucksef@gmail.com>
2020-01-30 14:39:03 -0700

    configured production environment to use sendgrid mailer through heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,11 +63,21 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "photo_app_#{Rails.env}"
 
+  # mailing shit goes here
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'safe-mesa-78631.herokuapp.com'
+  config.action_mailer.default_url_options = {host: host }
   config.action_mailer.perform_caching = false
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
