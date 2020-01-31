24edd7d02f0a1a6f3b97e8ead495d3efaf33b778
chucksef <chucksef@gmail.com>
2020-01-31 11:45:00 -0700

    Created functions to trigger a reset email being sent. Did not write functions to actually send the email yet

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,7 +1,21 @@
 class PasswordResetsController < ApplicationController
-  def new
-  end
+	def new
+	end
 
-  def edit
-  end
+	def create
+		@user = User.find_by(email: params[:password_reset][:email].downcase)
+
+		if @user
+			@user.create_reset_digest
+			@user.send_password_reset_email
+			flash[:info] = "Email Sent to #{@user.email} with Password Reset Instructions."
+			redirect_to root_url
+		else
+			flash.now[:danger] = "No User by that Email Address."
+			render :new
+		end
+	end
+
+	def edit
+	end
 end
