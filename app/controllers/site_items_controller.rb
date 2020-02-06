f57659429ef8da86aa85b2ea484f5732442c5134
chucksef <chucksef@gmail.com>
2020-02-06 07:13:24 -0700

    Fixed site_items#create. New Items are now added to the Site correctly

--- app/controllers/site_items_controller.rb
+++ app/controllers/site_items_controller.rb
@@ -15,13 +15,17 @@ class SiteItemsController < ApplicationController
 	end
 
 	def create
-		@item = @site.send(set_type.pluralize).new
+		@item = @site.send(set_type.pluralize).new(item_params)
 
-		@item.order = SiteItem.order(order: asc).last.order + 1
+		@item.order = SiteItem.order(order: :asc).last.order + 1
 		@item.site_id = 1
 
+		# 10.times { puts }
+		# puts "#{@item.class}:\n\nName: #{@item.name}\n\nDescription: #{@item.description}\n\nOrder: #{@item.order}\n\nSite_ID: #{@item.site_id}."
+		# 10.times { puts }
+
 		if @item.save
-			redirect_to site_items_index_path
+			redirect_to site_item_tmp_groups_path
 			flash[:success] = "#{params[:type].to_s[3..-1]} Successfully Created"
 		else
 			render :new
@@ -45,5 +49,9 @@ class SiteItemsController < ApplicationController
 			when "TmpCard"
 				"tmp_card"
 			end
+		end
+
+		def item_params
+			params.require(set_type.to_sym).permit(:type, :name, :subtitle, :visible, :description, :order, :site_id, :tmp_group_id, :article_1, :article_2, :article_3, :article_4, :article_5, :article_6, :heading_1, :heading_2, :heading_3, :heading_4, :heading_5, :heading_6)
 		end
 end
