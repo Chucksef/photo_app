7f2106bf7cd868194971e5194d82b6ac847ffe43
chucksef <chucksef@gmail.com>
2020-02-25 15:29:55 -0700

    title fixed

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,9 +1,7 @@
 class RoutesController < ApplicationController
 
     def root
-        puts "\n\n\n\n\n\n\n\n\n\n\n\n\n"
 		@item = Page.order(order: :asc).first
-		puts "....................#{@item.name}"
         render "pages/root"
     end
 
