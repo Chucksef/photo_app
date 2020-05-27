136f1d14a72afd309285f0137feabb3c6a953029
Chucksef <Chucksef@gmail.com>
2020-05-27 15:54:14 -0600

    added annoying page loaded test for production

--- app/assets/javascripts/splash.js
+++ app/assets/javascripts/splash.js
@@ -1,8 +1,14 @@
 // This page controls the events that will run when a user navigates
 // to the landing page (root) of the website.
 
-window.addEventListener("load", landingPage);
+// window.addEventListener("load", landingPage);
+window.onload = function() {
+    alert("page is loaded");
+};
 
 function landingPage() {
-    alert("testing");
+    let pth = document.querySelector("body").classList.toString();
+    if (pth == "routes root") {
+        alert("SPLASH!!!");
+    }
 }
