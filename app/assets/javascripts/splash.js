54062eb24fb6167d454f2c0a6aa9e3bec4baa611
Chucksef <Chucksef@gmail.com>
2020-05-27 16:05:51 -0600

    reverted annoying popup for production

--- app/assets/javascripts/splash.js
+++ app/assets/javascripts/splash.js
@@ -1,14 +1,14 @@
 // This page controls the events that will run when a user navigates
 // to the landing page (root) of the website.
 
-// window.addEventListener("load", landingPage);
-window.onload = function() {
-    alert("page is loaded");
-};
+window.addEventListener("turbolinks:load", landingPage);
 
 function landingPage() {
     let pth = document.querySelector("body").classList.toString();
     if (pth == "routes root") {
-        alert("SPLASH!!!");
+        
+        // THIS IS WHERE ALL LANDING PAGE CODE RUNS
+
+
     }
 }
